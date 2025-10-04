f7358ab872486026b4973fea6ecf7428b0d44eb4
john.zar <melow2@naver.com>
2025-10-04 14:40:22 +0900

    모노레포로 마이그레이션. 11

---
+++ chip_assets/lib/gen/colors.gen.dart
@@ -0,0 +1,238 @@
+// dart format width=80
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ChipColors {
+  ChipColors._();
+
+  /// Color: #FF149DE1
+  static const Color blueAlphaBase = Color(0xFF149DE1);
+
+  /// Color: #FF31BAE8
+  static const Color blueAlphaBaseDark = Color(0xFF31BAE8);
+
+  /// Color: #FFFFC9BB
+  static const Color brick200 = Color(0xFFFFC9BB);
+
+  /// Color: #FF5B2315
+  static const Color brick200Dark = Color(0xFF5B2315);
+
+  /// Color: #FFFFE9E3
+  static const Color brick50 = Color(0xFFFFE9E3);
+
+  /// Color: #FFCB4A2A
+  static const Color brick500 = Color(0xFFCB4A2A);
+
+  /// Color: #FFD25637
+  static const Color brick500Dark = Color(0xFFD25637);
+
+  /// Color: #FF36201A
+  static const Color brick50Dark = Color(0xFF36201A);
+
+  /// Color: #FFC13815
+  static const Color brick600 = Color(0xFFC13815);
+
+  /// Color: #FFD44A27
+  static const Color brick600Dark = Color(0xFFD44A27);
+
+  /// Color: #FFB03212
+  static const Color brick700 = Color(0xFFB03212);
+
+  /// Color: #FFB64023
+  static const Color brick700Dark = Color(0xFFB64023);
+
+  /// Color: #FFC2FFDB
+  static const Color green200 = Color(0xFFC2FFDB);
+
+  /// Color: #FF22603C
+  static const Color green200Dark = Color(0xFF22603C);
+
+  /// Color: #FFE0FFED
+  static const Color green50 = Color(0xFFE0FFED);
+
+  /// Color: #FF24F97D
+  static const Color green500 = Color(0xFF24F97D);
+
+  /// Color: #FF47FF94
+  static const Color green500Dark = Color(0xFF47FF94);
+
+  /// Color: #FF183825
+  static const Color green50Dark = Color(0xFF183825);
+
+  /// Color: #FF06E563
+  static const Color green600 = Color(0xFF06E563);
+
+  /// Color: #FF31ED7F
+  static const Color green600Dark = Color(0xFF31ED7F);
+
+  /// Color: #FF05C756
+  static const Color green700 = Color(0xFF05C756);
+
+  /// Color: #FF12D363
+  static const Color green700Dark = Color(0xFF12D363);
+
+  /// Color: #FF1EBE60
+  static const Color greenAlphaBase = Color(0xFF1EBE60);
+
+  /// Color: #FF59CC89
+  static const Color greenAlphaBaseDark = Color(0xFF59CC89);
+
+  /// Color: #FFFFFFFF
+  static const Color grey0 = Color(0xFFFFFFFF);
+
+  /// Color: #FF0F1215
+  static const Color grey0Dark = Color(0xFF0F1215);
+
+  /// Color: #FFEAECEE
+  static const Color grey100 = Color(0xFFEAECEE);
+
+  /// Color: #FF26292C
+  static const Color grey100Dark = Color(0xFF26292C);
+
+  /// Color: #FFDBE0E5
+  static const Color grey200 = Color(0xFFDBE0E5);
+
+  /// Color: #FF373B3E
+  static const Color grey200Dark = Color(0xFF373B3E);
+
+  /// Color: #FFCBD1D7
+  static const Color grey300 = Color(0xFFCBD1D7);
+
+  /// Color: #FF42474A
+  static const Color grey300Dark = Color(0xFF42474A);
+
+  /// Color: #FFAEB5BC
+  static const Color grey400 = Color(0xFFAEB5BC);
+
+  /// Color: #FF52585C
+  static const Color grey400Dark = Color(0xFF52585C);
+
+  /// Color: #FFF3F4F5
+  static const Color grey50 = Color(0xFFF3F4F5);
+
+  /// Color: #FF88919B
+  static const Color grey500 = Color(0xFF88919B);
+
+  /// Color: #FF767D82
+  static const Color grey500Dark = Color(0xFF767D82);
+
+  /// Color: #FF1A1C1E
+  static const Color grey50Dark = Color(0xFF1A1C1E);
+
+  /// Color: #FF68737E
+  static const Color grey600 = Color(0xFF68737E);
+
+  /// Color: #FF9EA4A8
+  static const Color grey600Dark = Color(0xFF9EA4A8);
+
+  /// Color: #FF464D53
+  static const Color grey700 = Color(0xFF464D53);
+
+  /// Color: #FFBCC1C4
+  static const Color grey700Dark = Color(0xFFBCC1C4);
+
+  /// Color: #FF2E3338
+  static const Color grey800 = Color(0xFF2E3338);
+
+  /// Color: #FFD7D9DB
+  static const Color grey800Dark = Color(0xFFD7D9DB);
+
+  /// Color: #FF161A1D
+  static const Color grey900 = Color(0xFF161A1D);
+
+  /// Color: #FFEFF0F1
+  static const Color grey900Dark = Color(0xFFEFF0F1);
+
+  /// Color: #FFC1BBFF
+  static const Color periwinkle200 = Color(0xFFC1BBFF);
+
+  /// Color: #FF170F6F
+  static const Color periwinkle200Dark = Color(0xFF170F6F);
+
+  /// Color: #FFE5E3FF
+  static const Color periwinkle50 = Color(0xFFE5E3FF);
+
+  /// Color: #FF5647FF
+  static const Color periwinkle500 = Color(0xFF5647FF);
+
+  /// Color: #FF5C5CFF
+  static const Color periwinkle500Dark = Color(0xFF5C5CFF);
+
+  /// Color: #FF110D3D
+  static const Color periwinkle50Dark = Color(0xFF110D3D);
+
+  /// Color: #FF3F2FEE
+  static const Color periwinkle600 = Color(0xFF3F2FEE);
+
+  /// Color: #FF4E3FF3
+  static const Color periwinkle600Dark = Color(0xFF4E3FF3);
+
+  /// Color: #FF3323E7
+  static const Color periwinkle700 = Color(0xFF3323E7);
+
+  /// Color: #FF4336D3
+  static const Color periwinkle700Dark = Color(0xFF4336D3);
+
+  /// Color: #FF149DE1
+  static const Color periwinkleAlphaBase = Color(0xFF149DE1);
+
+  /// Color: #FF31BAE8
+  static const Color periwinkleAlphaBaseDark = Color(0xFF31BAE8);
+
+  /// Color: #FFFFD1D1
+  static const Color red200 = Color(0xFFFFD1D1);
+
+  /// Color: #FF6B2E2E
+  static const Color red200Dark = Color(0xFF6B2E2E);
+
+  /// Color: #FFFFEBEB
+  static const Color red50 = Color(0xFFFFEBEB);
+
+  /// Color: #FFFF2E2E
+  static const Color red500 = Color(0xFFFF2E2E);
+
+  /// Color: #FFFF4C4C
+  static const Color red500Dark = Color(0xFFFF4C4C);
+
+  /// Color: #FF3E1E1E
+  static const Color red50Dark = Color(0xFF3E1E1E);
+
+  /// Color: #FFEF1010
+  static const Color red600 = Color(0xFFEF1010);
+
+  /// Color: #FFEE3F3F
+  static const Color red600Dark = Color(0xFFEE3F3F);
+
+  /// Color: #FFB30000
+  static const Color red700 = Color(0xFFB30000);
+
+  /// Color: #FFCA3838
+  static const Color red700Dark = Color(0xFFCA3838);
+
+  /// Color: #FFFC4848
+  static const Color redAlphaBase = Color(0xFFFC4848);
+
+  /// Color: #FFFF6060
+  static const Color redAlphaBaseDark = Color(0xFFFF6060);
+
+  /// Color: #000000
+  static const Color staticBlack = Color(0xFF000000);
+
+  /// Color: #FFFFFF
+  static const Color staticWhite = Color(0xFFFFFFFF);
+
+  /// Color: #FFFEBD00
+  static const Color yellowAlphaBase = Color(0xFFFEBD00);
+
+  /// Color: #FFFECE3D
+  static const Color yellowAlphaBaseDark = Color(0xFFFECE3D);
+}
