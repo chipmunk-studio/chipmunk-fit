0d7bd5a30bfd89584e4a869883bfa819643daf7f
john.zar <melow2@naver.com>
2026-01-11 22:41:13 +0900

    fittab수정.5

--- chip_component/lib/button/fit_animated_bottom_button.dart
+++ chip_component/lib/button/fit_animated_bottom_button.dart
@@ -1,15 +1,14 @@
-import 'dart:ui';
-
 import 'package:chip_component/button/fit_button.dart';
 import 'package:chip_foundation/buttonstyle.dart';
 import 'package:chip_foundation/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 /// 키보드 반응형 하단 버튼
 ///
 /// 키보드 상태에 따라 radius와 padding이 자동 애니메이션 처리됩니다.
-/// [WidgetsBindingObserver]를 통해 키보드 변화를 자체적으로 감지합니다.
+/// [KeyboardVisibilityBuilder]를 통해 키보드 변화를 자체적으로 감지합니다.
 ///
 /// ```dart
 /// FitAnimatedBottomButton(
@@ -19,7 +18,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 ///   child: Text('확인'),
 /// )
 /// ```
-class FitAnimatedBottomButton extends StatefulWidget {
+class FitAnimatedBottomButton extends StatelessWidget {
   const FitAnimatedBottomButton({
     super.key,
     required this.onPressed,
@@ -47,80 +46,53 @@ class FitAnimatedBottomButton extends StatefulWidget {
   /// 배경색 (기본: backgroundBase)
   final Color? backgroundColor;
 
-  @override
-  State<FitAnimatedBottomButton> createState() => _FitAnimatedBottomButtonState();
-}
-
-class _FitAnimatedBottomButtonState extends State<FitAnimatedBottomButton>
-    with WidgetsBindingObserver {
-  static const _kKeyboardThreshold = 50.0;
   static const _kAnimationDuration = Duration(milliseconds: 150);
-
-  double _keyboardHeight = 0;
-
-  bool get _isKeyboardVisible => _keyboardHeight > _kKeyboardThreshold;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    WidgetsBinding.instance.addPostFrameCallback((_) => _syncKeyboardHeight());
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeMetrics() => _syncKeyboardHeight();
-
-  void _syncKeyboardHeight() {
-    if (!mounted) return;
-    final newHeight = MediaQueryData.fromView(View.of(context)).viewInsets.bottom;
-    if (_keyboardHeight != newHeight) {
-      setState(() => _keyboardHeight = newHeight);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     final colors = context.fitColors;
-    final bgColor = widget.backgroundColor ?? colors.backgroundBase;
-    final bottomPadding = widget.useSafeArea
-        ? MediaQueryData.fromView(View.of(context)).padding.bottom
-        : 0.0;
+    final bgColor = backgroundColor ?? colors.backgroundBase;
+    final bottomPadding =
+        useSafeArea ? MediaQueryData.fromView(View.of(context)).padding.bottom : 0.0;
 
-    return TweenAnimationBuilder<double>(
-      duration: _kAnimationDuration,
-      curve: Curves.easeOutCubic,
-      tween: Tween(end: _isKeyboardVisible ? 0.0 : 1.0),
-      builder: (context, animValue, _) {
-        return Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: bgColor,
-            boxShadow: animValue > 0.5
-                ? [BoxShadow(color: bgColor.withValues(alpha: 0.08), blurRadius: 8, offset: const Offset(0, -2))]
-                : null,
-          ),
-          padding: EdgeInsets.only(
-            left: 20.w * animValue,
-            right: 20.w * animValue,
-            top: 12.h * animValue,
-            bottom: bottomPadding > 0 ? bottomPadding + 8 : 16.h * animValue,
-          ),
-          child: FitButton(
-            isExpanded: true,
-            type: widget.type,
-            isLoading: widget.isLoading,
-            loadingColor: widget.loadingColor ?? colors.violet500,
-            isEnabled: widget.isEnabled && !widget.isLoading,
-            style: _buildButtonStyle(animValue),
-            onPressed: widget.onPressed,
-            child: widget.child,
-          ),
+    return KeyboardVisibilityBuilder(
+      builder: (context, isKeyboardVisible) {
+        return TweenAnimationBuilder<double>(
+          duration: _kAnimationDuration,
+          curve: Curves.easeOutCubic,
+          tween: Tween(end: isKeyboardVisible ? 0.0 : 1.0),
+          builder: (context, animValue, _) {
+            return Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: bgColor,
+                boxShadow: animValue > 0.5
+                    ? [
+                        BoxShadow(
+                            color: bgColor.withValues(alpha: 0.08),
+                            blurRadius: 8,
+                            offset: const Offset(0, -2))
+                      ]
+                    : null,
+              ),
+              padding: EdgeInsets.only(
+                left: 20.w * animValue,
+                right: 20.w * animValue,
+                top: 12.h * animValue,
+                bottom: bottomPadding > 0 ? bottomPadding + 8 : 16.h * animValue,
+              ),
+              child: FitButton(
+                isExpanded: true,
+                type: type,
+                isLoading: isLoading,
+                loadingColor: loadingColor ?? colors.violet500,
+                isEnabled: isEnabled && !isLoading,
+                style: _buildButtonStyle(animValue),
+                onPressed: onPressed,
+                child: child,
+              ),
+            );
+          },
         );
       },
     );
@@ -132,6 +104,6 @@ class _FitAnimatedBottomButtonState extends State<FitAnimatedBottomButton>
         RoundedRectangleBorder(borderRadius: BorderRadius.circular(100.r * animValue)),
       ),
     );
-    return widget.style?.merge(animatedShape) ?? animatedShape;
+    return style?.merge(animatedShape) ?? animatedShape;
   }
 }
